3b8a2c6b4e4043f4bc1c8a81f13e82ee1488b8ea
soko-no-otaku <21125932+soko-no-otaku@users.noreply.github.com>
2023-11-23 14:45:47 +0900

    Return note in title tag

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import escape
+from flask import render_template
 
 import functions_framework
 
 @functions_framework.http
-def hello_http(request):
+def ogp_proxy(request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
@@ -13,13 +13,8 @@ def hello_http(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    request_json = request.get_json(silent=True)
     request_args = request.args
 
-    if request_json and "name" in request_json:
-        name = request_json["name"]
-    elif request_args and "name" in request_args:
-        name = request_args["name"]
-    else:
-        name = "World"
-    return f"Hello {escape(name)}!"
+    if request_args and "note" in request_args:
+        note = request_args["note"]
+    return render_template("ogp.html", note=note)
